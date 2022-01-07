7b2a84a8513d8a37e2629c6ff38b7ddc0bb14730
Busayo Samuel <busayosamuel2016@gmail.com>
2022-01-07 10:16:49 +0100

    added footer modal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import logo from "../assets/logo.png";
 import Uk from "../assets/Uk.png";
 import Nig from "../assets/Nigeria.png";
+import Modal from './Modal'
 const Footer = () => {
+
+  const [open, setOpen] = useState(false);
+  const [no1] = useState("+2348136181659");
+  const [no2] = useState("+447588067218");
+  const handleOpen=()=>{
+    setOpen(true)
+  }
   return (
     <div className="relative bottom-0 left-0 z-0 w-full">
       {" "}
@@ -12,18 +20,25 @@ const Footer = () => {
         <div className="text-xs md:text-base ml-2 sm:ml-6  font-medium w-full">
           <div className="inline-flex flex-wrap w-full">
             {" "}
-            You can contact us at
-            <span>
+            You can contact our Nigeria vendor
+            <span onClick={handleOpen} className="group-hover:animate-pulse cursor-pointer">
               <img src={Nig} alt="" className="mx-1 sm:mx-3 h-4 sm:h-6" />
             </span>
-            +2348136181659 or{" "}
-            <span>
-              <img src={Uk} alt="" className="mx-1 sm:mx-3 h-4 sm:h-6" />
+             or our UK vendor
+            <span onClick={handleOpen} className="group-hover:animate-pulse cursor-pointer">
+              <img src={Uk} alt="" className="mx-1 sm:mx-3 h-4 sm:h-6 group" />
             </span>{" "}
-            +447588067218
+           
           </div>
         </div>
       </div>
+      <Modal
+        open={open}
+        setOpen={setOpen}
+        no1={no1}
+        no2={no2}
+        message={'Hello, I just got off your website and I..'}
+      />
     </div>
   );
 };
